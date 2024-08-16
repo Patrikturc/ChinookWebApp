d54b5d1492a17d5b8c0c1a97d3aab0d0ae238e03
Patrik Turcani <pturcani@spartaglobal.com>
2024-08-16 11:07:27 +0100

    Added SecretKey gen

--- src/main/java/com/sparta/pt/chinookwebapp/ChinookWebAppApplication.java
+++ src/main/java/com/sparta/pt/chinookwebapp/ChinookWebAppApplication.java
@@ -3,11 +3,23 @@ package com.sparta.pt.chinookwebapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
 @SpringBootApplication
 public class ChinookWebAppApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoSuchAlgorithmException {
 		SpringApplication.run(ChinookWebAppApplication.class, args);
+//		generateKey();
 	}
 
+	private static void generateKey() throws NoSuchAlgorithmException {
+		KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+		SecretKey secretKey = keyGen.generateKey();
+		String encodedKey = Base64.getEncoder().encodeToString(secretKey.getEncoded());
+		System.out.println("KEY- \n" + encodedKey);
+	}
 }
